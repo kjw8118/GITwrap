6db147f87a1944649a3ea1f2c4f546a6d99f5183
kjw8118 <kjw8118@gmail.com>
2024-12-31 14:41:18 +0900

    git diff, git diff HEAD 간이 구현

---
+++ main.cpp
@@ -0,0 +1,20 @@
+﻿
+#include "GIT.h"
+
+
+#include <iostream>
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+
+    auto git = new GIT("./repo", "Jinwon", "kjw8118@gmail.com");
+    git->stagingAll();
+    std::cout << "Current status\n" << git->getCurrentStatus() << std::endl;
+    git->commitCurrentStage("init");
+    std::cout << "Current branch: " << git->getCurrentBranch() << std::endl;
+
+
+}
+
